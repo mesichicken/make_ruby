8b797ab0085fb794276ad2ab61a86edaa6fe1137
mesi <mesicharida202@gmail.com>
2024-01-12 22:26:46 +0900

    複文の実行

--- interp.rb
+++ interp.rb
@@ -18,14 +18,22 @@ def evaluate(tree)
     evaluate(tree[1]) > evaluate(tree[2])
   when '<'
     evaluate(tree[1]) < evaluate(tree[2])
+  when 'stmts'
+    i = 1
+    last = nil
+    while tree[i] != nil
+      last = evaluate(tree[i])
+      i = i + 1
+    end
+    last
+  when 'func_call'
+    p(evaluate(tree[2]))
   end
 end
 
-str = gets
+str = minruby_load()
 
 # 計算式の文字列をパースして、抽象構文木を生成する
 tree = minruby_parse(str)
 
-answer = evaluate(tree)
-
-p answer
+evaluate(tree)
