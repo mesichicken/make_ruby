ae08fca7553417130dc89640fc5993eda14d9e6b
mesi <mesicharida202@gmail.com>
2024-01-13 11:33:36 +0900

    変数を実装

--- interp.rb
+++ interp.rb
@@ -1,33 +1,37 @@
 require 'minruby'
 
-def evaluate(tree)
+def evaluate(tree, env)
   case tree[0]
   when 'lit'
     tree[1]
   when '+'
-    evaluate(tree[1]) + evaluate(tree[2])
+    evaluate(tree[1], env) + evaluate(tree[2], env)
   when '-'
-    evaluate(tree[1]) - evaluate(tree[2])
+    evaluate(tree[1], env) - evaluate(tree[2], env)
   when '*'
-    evaluate(tree[1]) * evaluate(tree[2])
+    evaluate(tree[1], env) * evaluate(tree[2], env)
   when '/'
-    evaluate(tree[1]) / evaluate(tree[2])
+    evaluate(tree[1], env) / evaluate(tree[2], env)
   when '=='
-    evaluate(tree[1]) == evaluate(tree[2])
+    evaluate(tree[1], env) == evaluate(tree[2], env)
   when '>'
-    evaluate(tree[1]) > evaluate(tree[2])
+    evaluate(tree[1], env) > evaluate(tree[2], env)
   when '<'
-    evaluate(tree[1]) < evaluate(tree[2])
+    evaluate(tree[1], env) < evaluate(tree[2], env)
   when 'stmts'
     i = 1
     last = nil
     while tree[i] != nil
-      last = evaluate(tree[i])
+      last = evaluate(tree[i], env)
       i = i + 1
     end
     last
+  when "var_assign"
+    env[tree[1]] = evaluate(tree[2], env)
+  when "var_ref"
+    env[tree[1]]
   when 'func_call'
-    p(evaluate(tree[2]))
+    p(evaluate(tree[2], env))
   end
 end
 
@@ -36,4 +40,5 @@ str = minruby_load()
 # 計算式の文字列をパースして、抽象構文木を生成する
 tree = minruby_parse(str)
 
-evaluate(tree)
+env = {}
+evaluate(tree, env)
